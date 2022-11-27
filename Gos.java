fd7167624a20bb8256529a8863a22ea4a76b691c
gerard.moreno@sportium.com <gerard.moreno@sportium.com>
2022-11-27 19:28:26 +0100

    Versio inicial G - Moreno

---
+++ Gos.java
@@ -0,0 +1,18 @@
+package Mamifers;
+
+public class Gos {
+	String raca;
+	String origen;
+
+	public void setRaca (String raca){
+		this.raca = raca;
+	}
+
+	public String getRaca(){
+		return raca;
+	}
+
+	public void setOrigen (String origen){
+		this.origen = origen;
+	}
+}
